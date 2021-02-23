0ba7d8d24c58b252491d1b3cf8a45b5786d23c60
Ted van den Brink <ted@digistate.nl>
2021-02-23 22:58:37 +0100

    added timing

--- .shortcuts/backup_dcim.sh
+++ .shortcuts/backup_dcim.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 set -xe
 
+start=`date +%s`
+
 (trap 'kill 0' SIGINT; \
   rsync --progress --partial -av --no-compress /storage/emulated/0/DCIM/Camera ted@brinksfiles:~/note9backup/internal_dcim & \
   rsync --progress --partial -av --no-compress /storage/1B21-8F9D/DCIM/Camera ted@brinksfiles:~/note9backup/external_dcim )
 
-termux-notification -t "Backup done!" -c "Rsync backup to brinksfiles completed sucessfully."
+end=`date +%s`
+runtime=$((end-start))
+
+termux-notification -t "Backup done!" -c "Rsync backup to brinksfiles completed sucessfully in $runtime seconds."
